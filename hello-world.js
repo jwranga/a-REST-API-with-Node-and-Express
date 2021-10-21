7d98261d5596d9cd110c6344a4981e6d9f7b4021
Ranga Lakshitha <jwranga@gmail.com>
2021-10-21 19:36:12 +0530

    .

--- hello-world.js
+++ hello-world.js
@@ -27,4 +27,42 @@ app.get('/books', (req, res) => {
     res.json(books);
 });
 
+app.get('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    for(let book of books){
+        if(book.isbn === isbn){
+            res.json(book);
+            return;
+        }
+    }
+    
+    res.status(404).send('book not found');
+});
+
+app.delete('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+
+    books = books.filter(i => {
+        if(i.isbn !== isbn){
+            return true;
+        }
+        return false;
+    });
+    
+    res.send('The book is deleted.');
+});
+
+app.post('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    for(let i=0; i < books.length; i++){
+        if(books[i].isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+
+    res.send('The book updated.');
+});
+
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
