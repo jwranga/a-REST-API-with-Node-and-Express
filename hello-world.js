50855f0b70c0c43528e4bf2479f139b6126e540e
Ranga Lakshitha <jwranga@gmail.com>
2021-10-21 17:42:06 +0530

    add two endpoints to store and get books.

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const corse = require('cors');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express.....');
+
+// where we will keep the books.
+let books = [];
+
+app.use(corse());
+
+// configure body perser middleware.
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+
+    // console.log(book);
+    books.push(book);
+    res.send('book is added to the database.');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
